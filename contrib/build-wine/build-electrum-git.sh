7127e60af08a12d6e8be857da443307506dbcf43
Pavel Pavlov <pavel.pavlov.itstf@gmail.com>
2018-08-15 20:24:31 +0300

    first commit

---
+++ contrib/build-wine/build-electrum-git.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# You probably need to update only this link
+ELECTRUM_GIT_URL=git://github.com/stratisproject/electrum-stratis.git
+BRANCH=master
+NAME_ROOT=electrum-stratis
+
+
+# These settings probably don't need any change
+export WINEPREFIX=/opt/wine64
+
+PYHOME=c:/python27
+PYTHON="wine $PYHOME/python.exe -OO -B"
+
+
+# Let's begin!
+cd `dirname $0`
+set -e
+
+cd tmp
+
+if [ -d "electrum-stratis-git" ]; then
+    # GIT repository found, update it
+    echo "Pull"
+    cd electrum-stratis-git
+    git checkout $BRANCH
+    git pull
+    cd ..
+else
+    # GIT repository not found, clone it
+    echo "Clone"
+    git clone -b $BRANCH $ELECTRUM_GIT_URL electrum-stratis-git
+fi
+
+cd electrum-stratis-git
+VERSION=`git describe --tags`
+echo "Last commit: $VERSION"
+
+cd ..
+
+rm -rf $WINEPREFIX/drive_c/electrum-stratis
+cp -r electrum-stratis-git $WINEPREFIX/drive_c/electrum-stratis
+cp electrum-stratis-git/LICENCE .
+
+# add python packages (built with make_packages)
+cp -r ../../../packages $WINEPREFIX/drive_c/electrum-stratis/
+
+# add locale dir
+cp -r ../../../lib/locale $WINEPREFIX/drive_c/electrum-stratis/lib/
+
+# Build Qt resources
+wine $WINEPREFIX/drive_c/Python27/Lib/site-packages/PyQt4/pyrcc4.exe C:/electrum-stratis/icons.qrc -o C:/electrum-stratis/lib/icons_rc.py
+wine $WINEPREFIX/drive_c/Python27/Lib/site-packages/PyQt4/pyrcc4.exe C:/electrum-stratis/icons.qrc -o C:/electrum-stratis/gui/qt/icons_rc.py
+
+cd ..
+
+rm -rf dist/
+
+# build standalone version
+$PYTHON "C:/pyinstaller/pyinstaller.py" --noconfirm --ascii -w deterministic.spec
+
+# build NSIS installer
+# $VERSION could be passed to the electrum.nsi script, but this would require some rewriting in the script iself.
+wine "$WINEPREFIX/drive_c/Program Files (x86)/NSIS/makensis.exe" /DPRODUCT_VERSION=$VERSION electrum.nsi
+
+cd dist
+mv electrum-stratis.exe $NAME_ROOT-$VERSION.exe
+mv electrum-stratis-setup.exe $NAME_ROOT-$VERSION-setup.exe
+mv electrum-stratis $NAME_ROOT-$VERSION
+zip -r $NAME_ROOT-$VERSION.zip $NAME_ROOT-$VERSION
+cd ..
+
+# build portable version
+cp portable.patch $WINEPREFIX/drive_c/electrum-stratis
+pushd $WINEPREFIX/drive_c/electrum-stratis
+patch < portable.patch 
+popd
+$PYTHON "C:/pyinstaller/pyinstaller.py" --noconfirm --ascii -w deterministic.spec
+cd dist
+mv electrum-stratis.exe $NAME_ROOT-$VERSION-portable.exe
+cd ..
+
+echo "Done."
---
+++ setup.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python2
+
+# python setup.py sdist --format=zip,gztar
+
+from setuptools import setup
+import os
+import sys
+import platform
+import imp
+import argparse
+
+version = imp.load_source('version', 'lib/version.py')
+
+if sys.version_info[:3] < (2, 7, 0):
+    sys.exit("Error: Electrum requires Python version >= 2.7.0...")
+
+data_files = []
+
+if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root=', dest='root_path', metavar='dir', default='/')
+    opts, _ = parser.parse_known_args(sys.argv[1:])
+    usr_share = os.path.join(sys.prefix, "share")
+    if not os.access(opts.root_path + usr_share, os.W_OK) and \
+       not os.access(opts.root_path, os.W_OK):
+        if 'XDG_DATA_HOME' in os.environ.keys():
+            usr_share = os.environ['XDG_DATA_HOME']
+        else:
+            usr_share = os.path.expanduser('~/.local/share')
+    data_files += [
+        (os.path.join(usr_share, 'applications/'), ['electrum-stratis.desktop']),
+        (os.path.join(usr_share, 'pixmaps/'), ['icons/electrum-stratis.png'])
+    ]
+
+setup(
+    name="Electrum-Stratis",
+    version=version.ELECTRUM_VERSION,
+    install_requires=[
+        'slowaes>=0.1a1',
+        'ecdsa>=0.9',
+        'pbkdf2',
+        'requests',
+        'qrcode',
+        'protobuf',
+        'dnspython',
+        'jsonrpclib',
+    ],
+    packages=[
+        'electrum_stratis',
+        'electrum_stratis_gui',
+        'electrum_stratis_gui.qt',
+        'electrum_stratis_plugins',
+        'electrum_stratis_plugins.audio_modem',
+        'electrum_stratis_plugins.cosigner_pool',
+        'electrum_stratis_plugins.email_requests',
+        'electrum_stratis_plugins.exchange_rate',
+        'electrum_stratis_plugins.hw_wallet',
+        'electrum_stratis_plugins.keepkey',
+        'electrum_stratis_plugins.labels',
+        'electrum_stratis_plugins.ledger',
+        'electrum_stratis_plugins.plot',
+        'electrum_stratis_plugins.trezor',
+        'electrum_stratis_plugins.virtualkeyboard',
+    ],
+    package_dir={
+        'electrum_stratis': 'lib',
+        'electrum_stratis_gui': 'gui',
+        'electrum_stratis_plugins': 'plugins',
+    },
+    package_data={
+        'electrum_stratis': [
+            'www/index.html',
+            'wordlist/*.txt',
+            'locale/*/LC_MESSAGES/electrum.mo',
+        ]
+    },
+    scripts=['electrum-stratis'],
+    data_files=data_files,
+    description="Lightweight Stratis Wallet",
+    author="dev0tion",
+    license="MIT Licence",
+    url="http://www.stratisplatform.com",
+    long_description="""Lightweight Stratis Wallet"""
+)
